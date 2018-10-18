db1f026f1ba1dd69d48fccb005c473308a75574a
Rodrigo Vargas <42876568+ravargas111@users.noreply.github.com>
2018-10-18 16:50:56 -0600

    prueba carrito

--- RutasPZ/src/rutaspz/controller/MapaController.java
+++ RutasPZ/src/rutaspz/controller/MapaController.java
@@ -7,13 +7,18 @@ package rutaspz.controller;
 
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
+import rutaspz.util.Animation;
 import rutaspz.util.AppContext;
 import rutaspz.util.Utils;
 
@@ -30,21 +35,70 @@ public class MapaController extends Controller implements Initializable {
     private AnchorPane apInterfaz;
     @FXML
     private JFXDrawer drawer;
-
+    private FontAwesomeIconView car;
+    private Double lFromX;
+    private Double lFromY;
+    private Double lToX;
+    private Double lToY;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        car=new FontAwesomeIconView(FontAwesomeIcon.CAR);
+        //car.set
+        apInterfaz.getChildren().add(car);
         Utils.getInstance().createHamburgerTransition((JFXHamburger) AppContext.getInstance().get("hambBtn"), drawer,"Menu");
         Utils.getInstance().createCloseDrawerEvent(drawer, apInterfaz);
+        initMouseEvent();
     }    
 
     @Override
     public void initialize() {
     }
     
+    private void initMouseEvent(){
+        this.apInterfaz.addEventHandler(MouseEvent.MOUSE_CLICKED, (e)->{
+            if(e.getButton()==MouseButton.PRIMARY)
+                leftClickEvent(e);
+            else if(e.getButton()==MouseButton.SECONDARY)
+                rigthClickEvent(e);
+            else if(e.getButton()==MouseButton.MIDDLE)
+                middleClickEvent(e); 
+        });
+    }
     
+    private void leftClickEvent(MouseEvent e){
+        lFromX= e.getX();
+        lFromY= e.getY();
+    }
+    
+    private void rigthClickEvent(MouseEvent e){
+        lToX= e.getX();
+        lToY= e.getY();
+        createLine(apInterfaz,lFromX,lToX,lFromY,lToY);
+        lFromX= e.getX();
+        lFromY= e.getY();
+    }
+    
+    private void middleClickEvent(MouseEvent e){
+        Animation.getInstance().desplazar(car,lFromX,lToX,lFromY,lToY);
+    }
+    
+    private void createLine(AnchorPane parent,Double fromX,Double toX,Double fromY,Double toY){
+        Line line=new Line();
+        parent.getChildren().add(line);
+        line.setStartX(fromX);
+        line.setEndX(toX);
+        line.setStartY(fromY);
+        line.setEndY(toY);
+        
+        //Animation.getInstance().desplazar(car,lFromX,lToX,lFromY,lToY);
+    }
+    
+    private void pruebaLineas(){
+        
+    }
     
     
 }
---
+++ RutasPZ/src/rutaspz/util/Animation.java
@@ -0,0 +1,377 @@
+package rutaspz.util;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import static javafx.animation.Animation.INDEFINITE;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+/**
+ * El siguiente single tone permite modelar la animaciones de manera
+ * que se ejecuten únicamente desde esta clase y darle más modularidad al
+ * software.
+ * @author Shinku
+ */
+
+
+public class Animation {
+    //Instancia de si mismo
+    private static final Animation SINGLETONE = new Animation();
+    
+    //Constructor básico
+    private Animation(){
+        //Carga los datos básicos
+    }
+    
+    //Getter de la instancia, es la única forma de acceso al SINGLETONE
+    public static Animation getInstance(){
+        return SINGLETONE;
+    }
+    
+    public void animarTexto(Node texto,String animación){
+        /**
+         * contiene las animaciones utilizadas en textos
+         * @param texto nodo de texto al cual se le aplica la animación
+         * @param animación nombre de animación a aplicar.
+         * @author Rodrigo Vargas
+         */
+        switch (animación){
+            case "escala":
+                animaciónEscala(texto);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void animarImagen(Node imagen,String animación){
+        /**
+         * contiene las animaciones utilizadas en textos
+         * @param imagen nodo de imagen al cual se le aplica la animación
+         * @param animación nombre de animación a aplicar
+         * @author Rodrigo Vargas
+         */
+        switch (animación){
+            case "desvanecimiento":
+                animaciónDesvanecimiento(imagen);
+                break;
+            case "escala":
+                animaciónEscala(imagen);
+                break;
+            case "horizontal":
+                animaciónHorizontal(imagen);
+                break;
+            case "vertical":
+                animaciónVertical(imagen);
+            default:
+                break;    
+        }
+        
+    } 
+    
+    public void animaciónDesvanecimiento(Node nodo){
+        FadeTransition Ft = new FadeTransition(Duration.millis(5000), nodo);
+        Ft.setFromValue(1.0);
+        Ft.setToValue(0.0);
+        Ft.play();
+    }
+    
+    public void animaciónEscala(Node nodo){
+        //Animación de escala
+        ScaleTransition St = new ScaleTransition(Duration.millis(3000), nodo);
+        //Se debe indicar la escala inicial y la final en los ejes X,Y y Z,
+        //segun se desee.
+        St.setFromX(1.0);
+        St.setFromY(1.0);
+        
+        St.setToX(0.9);
+        St.setToY(0.9);
+        //El auto reverse indica si la animación se realiza en dos sentidos
+        St.setAutoReverse(true);
+        /*
+        El Cyrcle count, indica la cantidad de veces que se repita la animación
+        En el caso de estar el auto reverse activado, se toma cada ciclo como
+        una animación independiente
+        */
+        St.setCycleCount(INDEFINITE);
+        //Inicia la animación
+        St.play();
+    }
+    
+    public void aumentarTamaño(Node nodo){
+        //Animación de escala
+        ScaleTransition St = new ScaleTransition(Duration.millis(700), nodo);
+        //Se debe indicar la escala inicial y la final en los ejes X,Y y Z,
+        //segun se desee.
+        St.setFromX(1.0);
+        St.setFromY(1.0);
+        
+        St.setToX(1.18);
+        St.setToY(1.18);
+        //El auto reverse indica si la animación se realiza en dos sentidos
+        St.setAutoReverse(true);
+        /*
+        El Cyrcle count, indica la cantidad de veces que se repita la animación
+        En el caso de estar el auto reverse activado, se toma cada ciclo como
+        una animación independiente
+        */
+        //Inicia la animación
+        St.play();
+    }
+    
+    public void disminuirTamaño(Node nodo){
+        //Animación de escala
+        ScaleTransition St = new ScaleTransition(Duration.millis(700), nodo);
+        //Se debe indicar la escala inicial y la final en los ejes X,Y y Z,
+        //segun se desee.
+        St.setFromX(1.18);
+        St.setFromY(1.18);
+        
+        St.setToX(1.0);
+        St.setToY(1.0);
+        //El auto reverse indica si la animación se realiza en dos sentidos
+        St.setAutoReverse(true);
+        /*
+        El Cyrcle count, indica la cantidad de veces que se repita la animación
+        En el caso de estar el auto reverse activado, se toma cada ciclo como
+        una animación independiente
+        */
+        //Inicia la animación
+        St.play();
+    }
+    
+    public void animaciónHorizontal(Node nodo){
+        TranslateTransition tt = new TranslateTransition(Duration.millis(2000), nodo);
+        tt.setByX(30f);//Especifica el valor incrementado de la coordenada X de parada, desde el comienzo, de este TranslateTransition
+        tt.setCycleCount(INDEFINITE);
+        tt.setAutoReverse(true);
+ 
+        tt.play();
+    }
+    
+    public void animaciónVertical(Node nodo){
+        TranslateTransition tt = new TranslateTransition(Duration.millis(2000), nodo);
+        tt.setByY(30f);//Especifica el valor incrementado de la coordenada X de parada, desde el comienzo, de este TranslateTransition
+        tt.setCycleCount(INDEFINITE);
+        tt.setAutoReverse(true);
+ 
+        tt.play();
+    }
+    
+    public void animaciónRotar(Node nodo,Double ánguloD){
+        RotateTransition rotar=new RotateTransition(Duration.seconds(3), nodo);
+        rotar.setFromAngle(0.0);
+        rotar.setToAngle(ánguloD);
+        rotar.setAutoReverse(true);
+        rotar.setCycleCount(INDEFINITE);
+        rotar.play();
+    }
+    
+    public void animaciónBoomerang(Node nodo){
+        TranslateTransition tt = new TranslateTransition(Duration.millis(2000), nodo);
+        tt.setByX(30f);//Especifica el valor incrementado de la coordenada X de parada, desde el comienzo, de este TranslateTransition
+        tt.setCycleCount(INDEFINITE);
+        tt.setAutoReverse(true);
+ 
+        RotateTransition rotar=new RotateTransition(Duration.seconds(3), nodo);
+        rotar.setFromAngle(0.0);
+        rotar.setToAngle(360.0);
+        rotar.setAutoReverse(true);
+        rotar.setCycleCount(INDEFINITE);
+        
+        ParallelTransition paralelo = new ParallelTransition();
+        paralelo.getChildren().addAll(
+           tt,rotar
+        );
+        
+        paralelo.play();
+    }
+    
+    public void animaciónCartaD(Node nodo,Image nuevaImg){
+        ScaleTransition disminuir = new ScaleTransition(Duration.millis(1000), nodo);
+        RotateTransition rotarDism=new RotateTransition(Duration.seconds(1), nodo);
+  
+        disminuir.setFromX(1.0);
+        disminuir.setFromY(1.0);
+        disminuir.setToX(0.0);
+        disminuir.setToY(0.5);
+        disminuir.setFromZ(1.0);
+        disminuir.setToZ(0.0);
+ 
+        disminuir.setAutoReverse(true);
+        
+        rotarDism.setFromAngle(0.0);
+        rotarDism.setToAngle(180.0);
+        //rotarDism.setAutoReverse(true);
+        
+        ParallelTransition paraleloDismim = new ParallelTransition();
+        paraleloDismim.getChildren().addAll(rotarDism,disminuir);
+        
+        //paraleloDismim.setOnFinished(value);
+        paraleloDismim.setOnFinished(new EventHandler<ActionEvent>(){
+ 
+            @Override
+            public void handle(ActionEvent arg0) {
+                ((ImageView)nodo).setImage(nuevaImg);
+            }
+        });
+        
+        paraleloDismim.play();     
+    }
+         
+    public void animaciónCartaA(Node nodo){
+        ScaleTransition aumentar = new ScaleTransition(Duration.millis(700), nodo);
+        RotateTransition rotarAum=new RotateTransition(Duration.seconds(0.7), nodo);
+        
+        rotarAum.setFromAngle(180.0);
+        rotarAum.setToAngle(0.0);
+        //rotarAum.setAutoReverse(true);
+        
+        aumentar.setFromX(0.0);
+        aumentar.setFromY(0.5);
+        aumentar.setToX(1.0);
+        aumentar.setToY(1.0);
+        aumentar.setFromZ(0.0);
+        aumentar.setToZ(1.0);
+        aumentar.setAutoReverse(true);
+        
+        ParallelTransition paraleloAumen = new ParallelTransition();
+        paraleloAumen.getChildren().addAll(rotarAum,aumentar);
+        paraleloAumen.setDelay(Duration.seconds(1.0));
+        paraleloAumen.play();
+    } 
+    
+    public void OcultaCarta(Node nodo,Image nuevaImg){
+        ScaleTransition disminuir = new ScaleTransition(Duration.millis(700), nodo);
+        RotateTransition rotarDism=new RotateTransition(Duration.seconds(0.7), nodo);
+  
+        disminuir.setFromX(1.0);
+        disminuir.setFromY(1.0);
+        disminuir.setToX(0.0);
+        disminuir.setToY(0.5);
+        disminuir.setFromZ(1.0);
+        disminuir.setToZ(0.0);
+ 
+        disminuir.setAutoReverse(true);
+        
+        rotarDism.setFromAngle(0.0);
+        rotarDism.setToAngle(180.0);
+        
+        ParallelTransition paraleloDismim = new ParallelTransition();
+        paraleloDismim.getChildren().addAll(rotarDism,disminuir);
+        
+        paraleloDismim.setOnFinished(new EventHandler<ActionEvent>(){
+ 
+            @Override
+            public void handle(ActionEvent arg0) {
+                ((ImageView)nodo).setImage(nuevaImg);
+                animaciónCartaA(nodo);
+            }
+        });
+        paraleloDismim.setDelay(Duration.seconds(3));
+        paraleloDismim.play();     
+    }
+    
+    public void voltearCarta(Node nodo,Image nuevaImg){
+        ScaleTransition disminuir = new ScaleTransition(Duration.millis(700), nodo);
+        RotateTransition rotarDism=new RotateTransition(Duration.seconds(0.7), nodo);
+  
+        disminuir.setFromX(1.0);
+        disminuir.setFromY(1.0);
+        disminuir.setToX(0.0);
+        disminuir.setToY(0.5);
+        disminuir.setFromZ(1.0);
+        disminuir.setToZ(0.0);
+ 
+        disminuir.setAutoReverse(true);
+        
+        rotarDism.setFromAngle(0.0);
+        rotarDism.setToAngle(180.0);
+        
+        ParallelTransition paraleloDismim = new ParallelTransition();
+        paraleloDismim.getChildren().addAll(rotarDism,disminuir);
+        
+        paraleloDismim.setOnFinished(new EventHandler<ActionEvent>(){
+ 
+            @Override
+            public void handle(ActionEvent arg0) {
+                ((ImageView)nodo).setImage(nuevaImg);
+                animaciónCartaA(nodo);
+            }
+        });
+        paraleloDismim.play(); 
+    }
+    
+    public void visibilidadMenu(Node menu,Boolean mostrar,Node button){
+        button.setDisable(true);
+        TranslateTransition visibilidad = new TranslateTransition(Duration.millis(1000), menu);
+        if(mostrar)
+        visibilidad.setByX(150f);
+        else
+            visibilidad.setByX(-150f);
+        visibilidad.play();
+        visibilidad.setOnFinished(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent arg0) {
+                button.setDisable(false);
+            }
+        
+        });
+    }
+    
+    public void desplazarVertical(Node nodo,Float desplazamiento,Boolean dirección){
+        TranslateTransition visibilidad = new TranslateTransition(Duration.millis(1000), nodo);
+        if(dirección)
+        visibilidad.setByY(desplazamiento);
+        else
+            visibilidad.setByY(-(desplazamiento));
+        visibilidad.play();
+    }
+    
+    public void desvanecimiento(Node nodo,Boolean mostrar){
+        if(mostrar){
+            FadeTransition desvanecer = new FadeTransition(Duration.millis(2000), nodo);
+            nodo.setVisible(true);
+            desvanecer.setFromValue(0.1);
+            desvanecer.setToValue(1.0);
+            desvanecer.play();
+            }
+        else{
+            FadeTransition desvanecer = new FadeTransition(Duration.millis(1000), nodo);
+            desvanecer.setFromValue(1.0);
+            desvanecer.setToValue(0.1);
+            desvanecer.setOnFinished(new EventHandler<ActionEvent>(){
+                @Override
+                public void handle(ActionEvent event) {
+                    nodo.setVisible(false);
+                }
+                
+            });
+            desvanecer.play();
+        }
+    }
+    
+    public void desplazar(Node nodo, Double fromX ,Double toX ,Double fromY,Double toY){
+        TranslateTransition desplazamiento = new TranslateTransition(Duration.millis(1000), nodo);
+        desplazamiento.setFromX(fromX);
+        desplazamiento.setToX(toX);
+        desplazamiento.setFromY(fromY);
+        desplazamiento.setToY(toY);
+        /*desplazamiento.setOnFinished(e->{
+                ((FontAwesomeIconView)nodo).setX(toX);
+                ((FontAwesomeIconView)nodo).setY(toY);
+        });*/
+        desplazamiento.play();
+    }
+    
+    
+    
+}
